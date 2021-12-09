49804b06720ec4044f3c34600a5044e20105a6b4
iyiola-dev <iyiola3332@gmail.com>
2021-12-09 23:14:01 +0100

    generae models and glqgen files

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,41 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/iyiola-dev/go-graphql/graph/generated"
+	"github.com/iyiola-dev/go-graphql/graph/model"
+)
+
+func (r *mutationResolver) CreatBook(ctx context.Context, input model.BookInput) (*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) DeleteBook(ctx context.Context, id int) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) UpdateBook(ctx context.Context, id int) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetAllBooks(ctx context.Context) ([]*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetOneBook(ctx context.Context, id int) (*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
