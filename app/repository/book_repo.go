a64da124828d09baa78256627ef54d7c0008213e
iyiola-dev <iyiola3332@gmail.com>
2021-12-12 00:43:03 +0100

    book repo

--- app/repository/book_repo.go
+++ app/repository/book_repo.go
@@ -6,7 +6,13 @@ import (
 	"gorm.io/gorm"
 )
 
-type BookRepository interface{}
+type BookRepository interface {
+	CreateBook(bookInput *model.BookInput) (*models.Book, error)
+	UpdateBook(bookInput *model.BookInput, id int) error
+	DeleteBook(id int) error
+	GetOneBook(id int) (*models.Book, error)
+	GetAllBooks() (*[]model.Book, error)
+}
 
 type BookService struct {
 	Db *gorm.DB
@@ -27,4 +33,33 @@ func (b *BookService) CreateBook(bookInput *model.BookInput) (*models.Book, erro
 	err := b.Db.Create(&book).Error
 
 	return book, err
+}
+func (b *BookService) UpdateBook(bookInput *model.BookInput, id int) error {
+	book := models.Book{
+		ID:        id,
+		Title:     bookInput.Title,
+		Author:    bookInput.Author,
+		Publisher: bookInput.Publisher,
+	}
+	err := b.Db.Model(&book).Where("id = ?", id).Updates(book).Error
+	return err
+}
+
+func (b *BookService) DeleteBook(id int) error {
+	book := &models.Book{}
+	err := b.Db.Delete(book, id).Error
+	return err
+}
+
+func (b *BookService) GetOneBook(id int) (*models.Book, error) {
+	book := &models.Book{}
+	err := b.Db.Where("id = ?", id).First(book).Error
+	return book, err
+}
+
+func (b *BookService) GetAllBooks() (*[]model.Book, error) {
+	books := []model.Book{}
+	err := b.Db.Find(&books).Error
+	return &books, err
+
 }
